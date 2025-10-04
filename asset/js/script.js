4d51ad38f417138076ffe958bded4a6abc68c728
ruis21 <ruinoa21@gmail.com>
2025-10-04 23:08:37 +0900

    -

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,9 @@
 $(function () {
+    alert("뽕");
     $(".hamburger-btn").on("click", function () {
-        $(".header-nav").stop().slideDown();
+        $(".header-nav").stop().slideDown(1000);
     });
     $(".close-btn").on("click", function () {
-        $(".header-nav").stop().slideUp();
+        $(".header-nav").stop().slideUp(1000);
     });
 });
